3a968773fb2199f1283c8e13001c10362dd36538
Ana Caroline <anacarolinesantos.contato@gmail.com>
2026-03-29 19:51:33 -0300

    Atualizando projeto To Do List

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 List<string> tarefas = new List<string>();
 while (true)
 {
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine(" To Do List");
+    Console.ResetColor();
+
     Console.WriteLine("\n--- To Do List ---");
     Console.WriteLine("1. Adicionar tarefa");
     Console.WriteLine("2. Listar tarefas");
@@ -25,7 +30,24 @@ while (true)
     {
         Console.WriteLine($"{i + 1}. {tarefas[i]}");
     }
-
     break;
+     case "3":
+     Console.Write("Digite o número da tarefa a ser removida: ");
+     if (int.TryParse(Console.ReadLine(), out int numeroTarefa )&& numeroTarefa > 0 && numeroTarefa <=tarefas.Count)
+     {
+         tarefas.RemoveAt(numeroTarefa - 1);
+         Console.WriteLine("Tarefa removida!");
+     }
+     else
+     {
+         Console.WriteLine("Número de tarefa inválido!");
+     }
+     break;
+     case "4":
+     Console.WriteLine("Saindo...");
+        return;
+        default:
+        Console.WriteLine("Opção inválida! Tente novamente.");
+        break;
 }
 }
